fb3b88d8b363b955a68daee3060835ee65cc07b6
brighit0805 <a20190802@pucp.edu.pe>
2024-09-10 21:07:15 -0500

    algo

---
+++ src/main/java/com/example/lab3_20190803/Lab320190803Application.java
@@ -0,0 +1,13 @@
+package com.example.lab3_20190803;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab320190803Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab320190803Application.class, args);
+    }
+
+}
